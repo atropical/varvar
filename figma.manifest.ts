3e3f0b64d77c1b2ae13d2aa03f59fc9508628ef3
Felipe Hefler <hefler@users.noreply.github.com>
2024-09-30 14:41:48 +0200

    fix: Moving `manifest.json` to dist folder

--- figma.manifest.ts
+++ figma.manifest.ts
@@ -1,9 +1,9 @@
-{
-  "name": "Varvar",
+export default {
+  "name": "VarVar",
   "id": "1341506281991381482",
   "api": "1.0.0",
-  "main": "dist/code.js",
-  "ui": "dist/src/index.html",
+  "main": "code.js",
+  "ui": "src/index.html",
   "editorType": ["figma", "dev"],
   "capabilities": ["inspect"],
   "menu": [{ "command": "export", "name": "Export Variables" }],
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import generateFile from "vite-plugin-generate-file";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import figmaManifest from "./figma.manifest";
 
 export default defineConfig({
   build: {
     root: "./src",
     target: 'esnext',
     outDir: "dist",
+    minify: "esbuild",
     emptyOutDir: true,
     assetsInlineLimit: 100000000,
     chunkSizeWarningLimit: 100000000,
+    cssMinify: true,
     cssCodeSplit: false,
     rollupOptions: {
-      input: "./src/index.html",
+      input: ["./src/index.html"],
       output: {
         inlineDynamicImports: true,
       },
@@ -21,5 +25,9 @@ export default defineConfig({
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
   },
-  plugins: [react(), viteSingleFile()],
+  plugins: [react(), viteSingleFile(), generateFile({
+    type: "json",
+    output: "./manifest.json",
+    data: figmaManifest,
+  })],
 });
